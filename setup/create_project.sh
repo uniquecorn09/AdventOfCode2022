266cb6f4f457a59a41cc1eec11e044a57b792961
jenny.stephan <jenny.stephan@de.ibm.com>
2022-12-04 17:56:18 +0100

    AoC22 #chore: Modify create_project

--- setup/create_project.sh
+++ setup/create_project.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-DAYS_OF_CODING=1
+DAYS_OF_CODING=2
 for (( i = 1; i <= $DAYS_OF_CODING; i++ )) 
 do 
     folder="day$i"
-    mkdir $folder
-    cd $folder
-    pwd
-    cp ../setup/_aoc.js aoc.js
-    cp ../setup/_aoc.test.js aoc.test.js
-    sed -i 's/DAY = 1/DAY = '$i'/' aoc.js
-    curl https://adventofcode.com/2022/day/$i > raw$i.html
-    touch data.txt
-    touch testdata.txt
-    cd ..
+    if [ -d "day$i" ]; then
+        echo "Project day$i exists"
+    else
+        mkdir $folder
+        cd $folder
+        pwd
+        cp ../setup/_aoc.js aoc.js
+        cp ../setup/_aoc.test.js aoc.test.js
+        sed -i 's/DAY = 1/DAY = '$i'/' aoc.js
+        curl https://adventofcode.com/2022/day/$i > raw$i.html
+        touch data.txt
+        touch testdata.txt
+        cd ..
+    fi
 done
